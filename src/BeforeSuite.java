fb5f61805bdf1ced0879008c22c1d955ee649251
pozdeyev <pozdeyev@gmail.com>
2019-03-31 20:11:14 +0300

    Lesson 7

---
+++ src/BeforeSuite.java
@@ -0,0 +1,13 @@
+
+//Выполняем до
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+
+public @interface BeforeSuite {
+}
--- src/lesson7_refleactAPI.java
+++
@@ -1,3 +0,0 @@
-public class lesson7_refleactAPI {
-
-}
